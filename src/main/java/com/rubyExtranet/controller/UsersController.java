bc3d96badd8f83d90173afb429c5f0f2e8fa5db4
ShdwBt <jhonyiglo@ecommerce.local>
2016-03-11 13:57:02 +0100

    first commit

---
+++ src/main/java/com/rubyExtranet/controller/UsersController.java
@@ -0,0 +1,14 @@
+package com.rubyExtranet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsersController {
+	
+	@RequestMapping("users")
+	public String users(){
+		return "users";
+	}
+	
+}
---
+++ src/main/java/com/rubyExtranet/model/User.java
@@ -0,0 +1,82 @@
+package com.rubyExtranet.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+ 
+    @Column(name="SSO_ID", unique=true, nullable=false)
+    private String ssoId;
+     
+    @Column(name="PASSWORD", nullable=false)
+    private String password;
+         
+    @Column(name="FIRST_NAME", nullable=false)
+    private String firstName;
+ 
+    @Column(name="LAST_NAME", nullable=false)
+    private String lastName;
+ 
+    @Column(name="EMAIL", nullable=false)
+    private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSsoId() {
+		return ssoId;
+	}
+
+	public void setSsoId(String ssoId) {
+		this.ssoId = ssoId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", ssoId=" + ssoId + ", password=" + password + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", email=" + email + "]";
+	}
+}
