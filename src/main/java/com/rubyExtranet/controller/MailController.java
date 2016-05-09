263c37fa9593d9e63f25b3348d7ca6980736095f
ShdwBt <ShdwBt@ShdwBt>
2016-05-09 14:54:46 +0200

    Mail Sender

---
+++ src/main/java/com/rubyExtranet/controller/MailController.java
@@ -0,0 +1,22 @@
+package com.rubyExtranet.controller;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rubyExtranet.mail.SmtpMailSender;
+
+@RestController
+public class MailController {
+	
+	@Autowired
+	private SmtpMailSender mailSender;
+	
+	@RequestMapping("/sendMail")
+	public void sendMail() throws MessagingException{
+		
+		mailSender.send("lite.team.asset@gmail.com", "Test mail Spring", "AssetBot");
+	}
+}
